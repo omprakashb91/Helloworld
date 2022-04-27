0ac231d15ac65469701599cc5259b7d15ed1ad5c
omprakashb91 <79237022+omprakashb91@users.noreply.github.com>
2022-04-28 00:49:12 +0530

    Add files via upload

---
+++ FirstProgram.c
@@ -0,0 +1,10 @@
+//Objective: C program to print Hello computer
+//Date     : 27/04/2021
+//Author   : Omprakash B
+#include <stdio.h>
+int main()
+{
+    printf("Hello, Computer Science Gurukul \n");
+    return 0;
+}
+
